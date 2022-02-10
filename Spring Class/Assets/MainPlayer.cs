94c2e858346debae0f47c87dfa5dac75e62e3fc6
Julian Mitton <jdmitton12@gmail.com>
2022-02-10 14:31:47 -0700

    Added myName

--- Spring Class/Assets/MainPlayer.cs
+++ Spring Class/Assets/MainPlayer.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MainPlayer : MonoBehaviour
 {
+    public string Julian;
+    
     // Use this for initialization
-    void Start() 
+    void Start()
     {
-        Debug.Log("I am alive!")
+        Debug.Log("I am alive and my name is " + Julian);
     }
 
     // Update is called once per frame
