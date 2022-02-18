9bbd4263fe2347d7335405c5824eb8590e4cbe99
Julian Mitton <jdmitton12@gmail.com>
2022-02-17 20:45:51 -0700

    Added True/False Operation

--- Variables and Operations Practice 2/Assets/Operations.cs
+++ Variables and Operations Practice 2/Assets/Operations.cs
@@ -8,6 +8,10 @@ namespace Operations
 		{
 			int x 170 + 30;
 			Console.WriteLine(x);
+			
+			int x = 8;
+			int y = 93;
+			Console.WriteLine(x < y); // returns true because 8 is less than 93
     	}
     }
 }
