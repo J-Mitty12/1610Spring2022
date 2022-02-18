71d33b2605bdbb62d9bca7abc6326705f8e42650
Julian Mitton <jdmitton12@gmail.com>
2022-02-17 20:56:07 -0700

    Added Sum Operation

---
+++ Variables and Operations Practice 2/Assets/Operations1.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operations1 : MonoBehaviour
+{
+    void Start()
+    {
+        int x = 300 + 100;
+        console.WriteLine(x);
+    }
+    void Update()
+    {
+        
+    }
+}
