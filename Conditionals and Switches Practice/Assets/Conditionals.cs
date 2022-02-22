cfd2eb229356fa518b461ef4e835d93f2988e8ef
Julian Mitton <jdmitton12@gmail.com>
2022-02-21 22:11:25 -0700

    Added If (x > y)

---
+++ Conditionals and Switches Practice/Assets/Conditionals.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    void Start()
+
+    {
+        int x = 35;
+        int y = 17;
+        if (x > y)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
