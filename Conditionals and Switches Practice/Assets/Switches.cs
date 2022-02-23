f749fd77d9dcd988cc59c2a1db8ea935c943b5cf
Julian Mitton <jdmitton12@gmail.com>
2022-02-22 17:11:41 -0700

    Added Switch Days

---
+++ Conditionals and Switches Practice/Assets/Switches.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyApplication;
+{
+	class Program
+	{
+		static void Main(String[] args)
+		{
+			int day = 2;
+			switch (day)
+			{
+				case 1:
+					Console.WriteLine("Monday");
+					break;
+				case 2:
+					Console.WriteLine("Tuesday");
+					break;
+			}
+		}
+	}
+}
