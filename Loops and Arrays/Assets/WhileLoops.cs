086b6aa628d0766b4b97a74f303a92337b35d6df
Julian Mitton <jdmitton12@gmail.com>
2022-03-03 20:28:14 -0700

    Added While Loop

---
+++ Loops and Arrays/Assets/WhileLoops.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Hello World
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 4;
+        while (i < 8)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
+
+}
